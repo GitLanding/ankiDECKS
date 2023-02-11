47d6002cf68cc55942d16dde537c6f2d3601ede5
GitLanding <111453341+GitLanding@users.noreply.github.com>
2023-02-11 17:09:41 +0000

    ASoC: msm: audio-effects: fix stack overread and heap overwrite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,2 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UserModule } from './user/user.module';
+import { PurchasesModule } from './payments/purchases.module';
+
+@Module({
+  imports: [
+    // TypeOrmModule.forRoot(),
+    ProductsModule,
+    UserModule,
+    PurchasesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/contracts/current-product.ts
@@ -0,0 +1,2 @@
+import { Product } from './product';
+import { ProductDetails } from './produ
