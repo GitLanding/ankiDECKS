5a57073f921ceb435f9bbd05a965454826375315
GitLanding <111453341+GitLanding@users.noreply.github.com>
2023-02-12 20:33:03 +0000

    Path fix

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
 
 import { AllProductsController } from './all-products';
-import { Current
+import { CurrentProductsController } from './current-products';
+import { PopProductsController } from './pop-products';
+import { RecommendedProductsController } from './recommended-products';
+import { ProductsService } from '../services/products';
+import { SearchProductController } from './search-product/search-product.controller';
+
+@Module({
+  controllers: [
+    PopProductsController,
+    AllProductsController,
+    CurrentProductsController,
+    RecommendedProductsController,
+    SearchProductController,
+  ],
+  providers: [ProductsService],
+})
+export class ProductsModule {}
